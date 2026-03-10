f8942ddfdb56a7519837cbbd793e62908444a9ad
PathPilot1112 <m22327803@gmail.com>
2026-03-10 15:33:05 +0530

    changed something

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,4 +1,4 @@
-public class UseCase1BookMyStayApp {
+public class BookMyStayApp {
     public static void main(String[] args){
         System.out.println("Welcome to the Hotel Booking Management  System");
         System.out.println("System initialised successfully");
