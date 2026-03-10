692ee15b6073c725ed52f005fdab5439643f7bb0
PathPilot1112 <m22327803@gmail.com>
2026-03-10 15:29:21 +0530

    Usecase 2

--- src/UseCase1BookMyStayApp.java
+++
@@ -1,6 +0,0 @@
-public class UseCase1BookMyStayApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to the Hotel Booking Management  System");
-        System.out.println("System initialised successfully");
-    }
-}
